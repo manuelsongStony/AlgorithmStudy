9734e42bad535589c6209e9998ef9b7d7deb9b83
ManuelSong <manuel.song@stonybrook.edu>
2022-03-23 00:22:14 +0900

    kruskal algorithm

--- Graph/kruskal-algorithm.py
+++ Graph/kruskal-algorithm.py
@@ -17,19 +17,24 @@ def union_parent(parent, a, b):
 v, e = map(int, input().split())
 parent = [0] * (v + 1)
 
+edges=[]
+result=0
+
 for i in range(1, v + 1):
     parent[i] = i
 
-for i in range(e):
-    a, b = map(int, input().split())
-    union_parent(parent, a, b)
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost,a,b))
 
-print('각 원소가 속한 집합:', end=' ')
-for i in range(1, v + 1):
-    print(find_parent(parent, i), end=' ')
 
-print()
+edges.sort()
 
-print('부모 테이블: ', end=' ')
-for i in range(1, v + 1):
-    print(parent[i], end=' ')
+for edge in edges:
+    cost, a, b= edge
+
+    if find_parent(parent,a)!= find_parent(parent,b):
+        union_parent(parent,a ,b)
+        result += cost
+
+print(result)
