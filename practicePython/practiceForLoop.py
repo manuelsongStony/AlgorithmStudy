1dcaf3d93f8c8a2681684aaa74d3fee143762009
ManuelSong <manuel.song@stonybrook.edu>
2022-03-24 21:22:46 +0900

    union-find algorithm problem 1

--- practicePython/practiceForLoop.py
+++ practicePython/practiceForLoop.py
@@ -2,3 +2,19 @@ for i in range(10):
     if 1<2:
         continue
     print("sd")
+
+n=10
+"""
+for i in n:
+    print(i, end=' ')
+"""
+
+
+for i in range(n):
+    print(i, end=' ')
+
+print()
+
+for i in range(0,n):
+    print(i, end=' ')
+
