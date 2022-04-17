4079be2bef6a1c8dedbcb108d63a335e37233d6d
ManuelSong <manuel.song@stonybrook.edu>
2022-04-18 01:01:30 +0900

    example10-try-2

--- This-Is-Coding-Test-Example/example-10-implement-try.py
+++ This-Is-Coding-Test-Example/example-10-implement-try.py
@@ -1,51 +1,109 @@
+import copy
+
+[0, 1, 0]
+[1, 0, 0]
+[1, 0, 0]
+
+[(0, 0), (0, 0), (0, 0), (0, 0)]
+[(0, 0), (0, 0), (0, 0), (0, 0)]
+[(0, 0), (0, 0), (0, 0), (0, 0)]
+[(0, 0), (0, 0), (0, 0), (0, 0)]
+
+key2 = \
+    [[(0, 0), (0, 1), (0, 2), (0, 3)],
+     [(1, 0), (1, 1), (1, 2), (1, 3)],
+     [(2, 0), (2, 1), (2, 2), (2, 3)],
+     [(3, 0), (3, 1), (3, 2), (3, 3)]]
+
+[(3, 0), (2, 0), (1, 0), (0, 0)]
+[(3, 1), (2, 1), (1, 1), (0, 1)]
+[(3, 2), (2, 2), (1, 2), (0, 2)]
+[(3, 3), (2, 3), (1, 3), (0, 3)]
+
+key = [[0, 0, 0],
+       [1, 0, 0],
+       [0, 1, 1]]
+
+lock = [[1, 1, 1],
+        [1, 1, 0],
+        [1, 0, 1]]
+
+key1 = [[0, 0, 0],
+       [0, 0, 0],
+       [0, 0, 0]]
+
+lock1 = [[1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]]
+
+key2 = [[1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]]
+
+lock2 = [[0, 0, 0],
+       [0, 0, 0],
+       [0, 0, 0]]
+
+
 def turn_ninty(array):
-    #temp=[[0 for _ in range(len(array))] for _ in range(len(array))]
-    #temp1=[[0]*4]*4
-    n=len(array)
-    temp2 = [[0]*n for _ in range(n)]
+    # temp=[[0 for _ in range(len(array))] for _ in range(len(array))]
+    # temp1=[[0]*4]*4
+    n = len(array)
+    temp2 = [[0] * n for _ in range(n)]
 
     for i in range(n):
         for j in range(n):
-            temp2[j][n-1-i]=array[i][j]
+            temp2[j][n - 1 - i] = array[i][j]
 
     return temp2
 
 
-def check_key(array):
-    if
+def enlarge_lock(key, lock):
+    extra = len(key) - 1
 
-key=[[0,0,0],
-     [1,0,0],
-     [0,1,1]]
+    new_lock = [[0] * (extra * 2 + len(lock)) for _ in range(extra * 2 + len(lock))]
 
+    for i in range(len(lock)):
+        for j in range(len(lock)):
+            new_lock[i + extra][j + extra] = lock[i][j]
 
-lock=[[1,1,1],
-      [1,1,0],
-      [1,0,1]]
+    return new_lock
 
+def check_valid(array,key):
+    extra=len(key) - 1
+    for row in range(len(array)-(2*extra)):
+        for col in range(len(array)-(2*extra)):
+            if array[row+extra][col+extra]==1:
+                continue
+            else:
+                return False
+    return True
 
-[0,1,0]
-[1,0,0]
-[1,0,0]
+def check(key, lock):
+    e_lock=enlarge_lock(key, lock)
 
-[(0,0),(0,0),(0,0),(0,0)]
-[(0,0),(0,0),(0,0),(0,0)]
-[(0,0),(0,0),(0,0),(0,0)]
-[(0,0),(0,0),(0,0),(0,0)]
+    n = len(e_lock)-len(key)+1
 
-key2=\
-    [[(0,0),(0,1),(0,2),(0,3)],
-    [(1,0),(1,1),(1,2),(1,3)],
-    [(2,0),(2,1),(2,2),(2,3)],
-    [(3,0),(3,1),(3,2),(3,3)]]
+    for i in range(4):
+        key=turn_ninty(key)
+        for i in range(n):
+            for j in range(n):
+                temp=copy.deepcopy(e_lock)
+                for row in range(len(key)):
+                    for col in range(len(key)):
+                        temp[i+row][j+col]+=key[row][col]
 
-[(3,0),(2,0),(1,0),(0,0)]
-[(3,1),(2,1),(1,1),(0,1)]
-[(3,2),(2,2),(1,2),(0,2)]
-[(3,3),(2,3),(1,3),(0,3)]
+                """for k in temp:
+                    print(k)
+                print()"""
+                if check_valid(temp,key):
+                    return True
 
+    return False
 
-
-
-for i in turn_ninty(key2):
+print(check(key, lock))
+"""
+for i in enlarge_lock(key, lock):
     print(i)
+"""
+
